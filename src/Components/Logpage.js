89182bba0a246f0393a6d6942da8b33f605a1231
Tharindu De Silva <tharindu.2019032@iit.ac.lk>
2022-07-25 15:27:19 +0530

    Routing Completed

--- src/Components/Logpage.js
+++ src/Components/Logpage.js
@@ -1,34 +1,40 @@
 import React from 'react'
 import { Button } from 'rebass'
 import { Input } from '@rebass/forms'
+import { Route, Routes } from 'react-router-dom'
 
 const Logpage = () => {
   return (
     <form className='formContent'>
-        <h2 className='formHeading'>Login to pick a challenge</h2>
-        
-        <div className='formInput'>
-        <Input
-        id='email'
-        name='email'
-        type='email'
-        placeholder='jane@example.com'
-      />
-        </div>
+      <h2 className='formHeading'>Login to pick a challenge</h2>
 
-        <div className='formInput'>
+      <div className='formInput'>
         <Input
-        id='password'
-        name='password'
-        type='password'
-        placeholder='***********'
-      />
-        </div>
+          id='email'
+          name='email'
+          type='email'
+          placeholder='jane@example.com'
+        />
+      </div>
 
-        <Button variant='primary' mr={2} className='btn'>Login</Button>
+      <div className='formInput'>
+        <Input
+          id='password'
+          name='password'
+          type='password'
+          placeholder='***********'
+        />
+      </div>
+        <Button variant='primary' className='btn' >Login</Button>
     </form>
 
   )
+
+  // function show (){
+  //   <Routes>
+  //       <Route path="/" element={<Logpage />}></Route>
+  //   </Routes>
+  // }
 }
 
 export default Logpage
--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router , Route, Routes } from "react-router-dom";
 import './index.css';
 import Logpage from './Components/Logpage';
 
 
 const HelloWorld = () => {
     return (
-        <Logpage />
+        <Router>
+            <Routes>
+                <Route path="/" element={<Logpage />}></Route>
+            </Routes>
+        </Router>
     );
 }
 
